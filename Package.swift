fa6ea9f17f321ba759146b27687816ec7baf5b1a
Helbert Gomes <helbertgs@gmail.com>
2026-07-30 19:41:24 -0400

    fix (windows): add missing win32_window.c to Package.swift

--- Package.swift
+++ Package.swift
@@ -62,7 +62,7 @@ fileprivate enum OS {
     private var windowsSources : [String] {[
         "win32_init.c", "win32_joystick.c", "win32_joystick.h", "win32_module.c", 
         "win32_monitor.c", "win32_platform.h", "win32_thread.c", "win32_thread.h", 
-        "win32_time.c", "win32_time.h", "wgl_context.c"
+        "win32_time.c", "win32_time.h", "wgl_context.c", "win32_window.c"
     ]}
 
     private var linuxSources : [String] {[
